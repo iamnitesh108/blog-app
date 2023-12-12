6b53dcbedbb3cda1c9163c84943b4367d5f6f47a
iamnitesh108 <iamnitesh108@gmail.com>
2023-12-12 14:48:03 +0530

    react blog-app project complemented

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -1,48 +1,42 @@
-import conf from "../conf/conf.js"
+import conf from "../conf/conf.js";
 import { Client, Account, ID } from "appwrite";
 
-export class AuthService {
-  client = new Client();
-  account;
-
+class AuthService {
   constructor() {
-    this.client
-        .setEndpoint(conf.appwriteUrl)
-        .setProject(conf.appwriteProjectId);
-    this.account = new Account(this.client)
+    this.client = new Client()
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.account = new Account(this.client);
   }
 
-  async createAccount({email, password, name}) {
+  async createAccount({ email, password, name }) {
     try {
       const userAccount = await this.account.create(ID.unique(), email, password, name);
 
       if (userAccount) {
-       return this.login({email, password});
+        return this.login({ email, password });
       } else {
         return userAccount;
       }
-
     } catch (error) {
       throw error;
     }
   }
 
-  async login({email, password}) {
+  async login({ email, password }) {
     try {
-      return await this.account.createEmailSession(email, password)
+      return await this.account.createSession(email, password);
     } catch (error) {
       throw error;
     }
-
-    return null;
   }
 
   async getCurrentUser() {
     try {
       return await this.account.get();
     } catch (error) {
-      console.log("Appwrite service :: getCurrentUser :: error", error)
-
+      console.error("Appwrite service :: getCurrentUser :: error", error);
+      throw error;
     }
   }
 
@@ -50,11 +44,11 @@ export class AuthService {
     try {
       await this.account.deleteSessions();
     } catch (error) {
-      console.log("Appwrite service :: logout :: error", error)
+      console.error("Appwrite service :: logout :: error", error);
+      throw error;
     }
   }
 }
-
 
 const authService = new AuthService();
 
